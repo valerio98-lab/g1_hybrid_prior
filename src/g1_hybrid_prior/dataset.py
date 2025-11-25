3d481df58d6f475c52c21d978055a4b85bdd1d27
Vale937 <valeriobelli26@gmail.com>
2025-11-25 15:25:01 +0100

    lazy loading logic done plus central difference for linear and angular velocity

--- src/g1_hybrid_prior/dataset.py
+++ src/g1_hybrid_prior/dataset.py
@@ -1,10 +1,10 @@
 
 from torch.utils.data import Dataset
 from pathlib import Path
-import numpy as np
 import torch
+import numpy as np
 from .robot_cfg import RobotCfg, load_robot_cfg
-from .helpers import get_project_root, quat_normalize, quat_mul, quat_inv, quat_log
+from .helpers import get_project_root, quat_normalize, quat_mul, quat_inv, quat_log, wrap_to_pi, rotate_world_to_body
 
 
 class G1HybridPriorDataset(Dataset):
@@ -20,58 +20,50 @@ class G1HybridPriorDataset(Dataset):
         self.lazy_load = lazy_load
         self.lazy_load_window = lazy_load_window
         self.block_index = 0
-        self.device = "cuda" if device is None and torch.cuda.is_available() else device
+        self.device = "cuda" if (device is None and torch.cuda.is_available()) else device or "cpu"
 
         self.__load_data__(file_path)
 
     def __load_data__(self, file_path):
         if self.lazy_load:
-            self.__lazy_load_data__(file_path)
+            data = np.loadtxt(file_path, delimiter=",", max_rows=self.lazy_load_window, skiprows=(self.block_index * self.lazy_load_window + 1))
+            self.block_index += 1
+        
         else: 
-            # Implement full data loading logic here
             data = np.loadtxt(file_path, delimiter=",")
-            if data.shape[1] != self.robot_cfg.expected_cols:
-                raise ValueError(
-                    f"CSV has {data.shape[1]} cols, but {self.robot_cfg.name} expects "
-                    f"{self.robot_cfg.expected_cols} (root {self.robot_cfg.root_dim} + dof {self.robot_cfg.dof})."
-                )
-            self.data = data
-            for row in data:
-                root_pos, root_quat_xyzw, root_quat_wxyz, joints = self.__split_row__(row, self.robot_cfg)
-                frame = {
-                    "root_pos": root_pos,
-                    "root_quat_xyzw": root_quat_xyzw,
-                    "root_quat_wxyz": root_quat_wxyz,
-                    "joints": joints,
-                    "root_vel": None,
-                    "joint_vel": None
-                }
-                self.dataset.append(frame)
-                
-    def __lazy_load_data__(self, file_path):
-        data = np.loadtxt(file_path, delimiter=",", max_rows=self.lazy_load_window, skiprows=(self.block_index * self.lazy_load_window + 1))
+        
+        if data.shape[1] != self.robot_cfg.expected_cols:
+            raise ValueError(
+                f"CSV has {data.shape[1]} cols, but {self.robot_cfg.name} expects "
+                f"{self.robot_cfg.expected_cols} (root {self.robot_cfg.root_dim} + dof {self.robot_cfg.dof})."
+            )
         self.data = data
-        self.block_index += 1
-        for row in range(data.shape[0]):
-            root_pos, root_quat_xyzw, root_quat_wxyz, joints = self.__split_row__(data[row], self.robot_cfg)
-            next_root_pos, next_root_quat_xyzw, next_root_quat_wxyz, next_joints = self.__split_row__(data[row+1], self.robot_cfg)
-            root_lin_vel = root_pos
-            root_ang_vel = root_quat_wxyz
-            joint_velocities = joints
-            if row != data.shape[0]-1 or row != 0:
-                root_lin_vel, root_ang_vel, joint_velocities = self.__compute_velocities__(
-                    root_pos, 
-                    root_quat_wxyz, 
-                    joints, 
-                    next_root_pos, 
-                    next_root_quat_wxyz, 
-                    next_joints
-                )
+        
+        self.__frame_building__(file_path, data)
+
+                
+    def __frame_building__(self, file_path, data):
+        for row in range(self.data.shape[0]):
+            cur = self.__split_row__(data[row], self.robot_cfg)
+            if 0 < row < data.shape[0]-1:
+                nxt = self.__split_row__(data[row+1], self.robot_cfg)
+                prev = self.__split_row__(data[row-1], self.robot_cfg)
+            elif row == 0:
+                nxt = self.__split_row__(data[row+1], self.robot_cfg)
+                prev = cur  
+            else:  # row == data.shape[0]-1
+                prev = self.__split_row__(data[row-1], self.robot_cfg)
+                nxt = cur 
+
+            root_lin_vel, root_ang_vel, joint_velocities = self.__compute_velocities__(
+                nxt[0], nxt[1], nxt[2],   # next_root_pos, next_root_quat_wxyz, next_joints
+                prev[0], prev[1], prev[2],   # prev_root_pos, prev_root_quat_wxyz, prev_joints
+            )
+
             frame = {
-                "root_pos": root_pos,
-                "root_quat_xyzw": root_quat_xyzw,
-                "root_quat_wxyz": root_quat_wxyz,
-                "joints": joints,
+                "root_pos": cur[0],
+                "root_quat_wxyz": cur[1],
+                "joints": cur[2],
                 "root_lin_vel": root_lin_vel,
                 "root_ang_vel": root_ang_vel,
                 "joint_vel": joint_velocities
@@ -80,19 +72,28 @@ class G1HybridPriorDataset(Dataset):
 
 
     def __split_row__(self, row: np.ndarray, robot_cfg: RobotCfg) -> tuple:
-
         root = row[: robot_cfg.root_dim]
         joints = row[robot_cfg.root_dim : robot_cfg.dof + robot_cfg.root_dim]
 
         qx, qy, qz, qw = root[3:7]
-        root_pos = root[0:3]
-        root_quat_xyzw = np.array([qx, qy, qz, qw], dtype=row.dtype)  # x,y,z,w
-        root_quat_wxyz = np.array([qw, qx, qy, qz], dtype=row.dtype)  # w,x,y,z
+        root_pos = torch.tensor(root[0:3], dtype=torch.float32)
+        root_quat_wxyz = torch.tensor([qw, qx, qy, qz], dtype=torch.float32)  # w,x,y,z
+        joints = torch.tensor(joints, dtype=torch.float32)
+        root_quat_wxyz = quat_normalize(root_quat_wxyz)
 
-        return root_pos, root_quat_xyzw, root_quat_wxyz, joints
+        return root_pos, root_quat_wxyz, joints
 
-    def __compute_velocities__(self, root_pos, root_quat_wxyz, joints, next_root_pos, next_root_quat_wxyz, next_joints): 
+    def __compute_velocities__(self, next_root_pos, next_root_quat_wxyz, next_joints, prev_root_pos, prev_root_quat_wxyz, prev_joints): 
         dt = 1.0 / self.robot_cfg.fps
+        root_lin_vel = (next_root_pos - prev_root_pos) / (2 * dt) ## Central difference
+        q_prev_inv = quat_inv(prev_root_quat_wxyz)
+        q_rel = quat_mul(q_prev_inv, next_root_quat_wxyz)
+        q_rel = quat_normalize(q_rel)
+        q_prev_norm = quat_normalize(prev_root_quat_wxyz)
+        root_lin_vel = rotate_world_to_body(root_lin_vel, q_prev_norm)
+        root_ang_vel = quat_log(q_rel) / (2 * dt)
+        joint_velocities = wrap_to_pi(next_joints - prev_joints) / (2 * dt)
+        return root_lin_vel, root_ang_vel, joint_velocities
         
 
 
@@ -103,7 +104,7 @@ class G1HybridPriorDataset(Dataset):
         if self.lazy_load: 
             if idx > len(self.dataset):
                 self.dataset = []
-                self.__lazy_load_data__(self.file_path)
+                self.__load_data__(self.file_path)
             block_idx = (idx - self.block_index * self.lazy_load_window)+1
             return self.dataset[block_idx]
         else:
--- src/g1_hybrid_prior/helpers.py
+++ src/g1_hybrid_prior/helpers.py
@@ -9,12 +9,21 @@ def get_project_root():
 
 
 def wrap_to_pi(x: torch.Tensor) -> torch.Tensor:
+    """
+    Wraps the input tensor values to the range [-pi, pi].
+    """
     return (x + math.pi) % (2 * math.pi) - math.pi
 
 def quat_normalize(q: torch.Tensor, eps=1e-8) -> torch.Tensor:
+    """
+    Normalizes a quaternion tensor to have unit norm.
+    """
     return q / q.norm(dim=-1, keepdim=True).clamp_min(eps)
 
 def quat_mul(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
+    """
+    Multiplies two quaternion tensors.
+    """
     # q = [w, x, y, z]
     w1, x1, y1, z1 = q1.unbind(-1)
     w2, x2, y2, z2 = q2.unbind(-1)
@@ -25,10 +34,16 @@ def quat_mul(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
     return torch.stack([w, x, y, z], dim=-1)
 
 def quat_inv(q: torch.Tensor) -> torch.Tensor:
+    """
+    Computes the inverse of a quaternion tensor.
+    """
     w, x, y, z = q.unbind(-1)
     return torch.stack([w, -x, -y, -z], dim=-1)
 
 def quat_log(q: torch.Tensor, eps=1e-8) -> torch.Tensor:
+    """
+    Computes the logarithm of a quaternion tensor.
+    """
     q = quat_normalize(q, eps)
     w = q[..., :1]
     v = q[..., 1:]
@@ -36,3 +51,17 @@ def quat_log(q: torch.Tensor, eps=1e-8) -> torch.Tensor:
     theta = torch.atan2(v_norm, w.clamp(-1+1e-8, 1-1e-8))
     axis = v / v_norm
     return 2.0 * theta * axis  
+
+
+def rotate_world_to_body(v_world: torch.Tensor, q_wxyz: torch.Tensor) -> torch.Tensor:
+    """
+    Rotates a vector from world coordinates to body coordinates using the given quaternion.
+    """
+    w, x, y, z = q_wxyz.unbind(-1)
+    R = torch.stack([
+        1 - 2*(y*y + z*z), 2*(x*y - z*w),     2*(x*z + y*w),
+        2*(x*y + z*w),     1 - 2*(x*x + z*z), 2*(y*z - x*w),
+        2*(x*z - y*w),     2*(y*z + x*w),     1 - 2*(x*x + y*y)
+    ], dim=-1).reshape(3,3)
+    return R.t().matmul(v_world)  # (3,)
+
